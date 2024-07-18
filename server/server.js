2c6e559335c58bfcc084ddce9213441da3e6c80b
jaideep190 <ajaiajai710@gmail.com>
2024-07-18 18:29:19 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -14,7 +14,18 @@ const io = socketIo(server, {
   }
 })
 
-app.use(cors())
+const allowedOrigins = ['https://pongmobile-y8qo.onrender.com']
+app.use(cors({
+  origin: function(origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  }
+}))
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
