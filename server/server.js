ccee8bbda0025feb78f65e7ce683ed3c1d637c03
jaideep190 <ajaiajai710@gmail.com>
2024-07-18 18:05:04 +0530

    Endpoints Updated

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express()
 const server = http.createServer(app)
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://pong-mobile.vercel.app",
     methods: ["GET", "POST"]
   }
 })
